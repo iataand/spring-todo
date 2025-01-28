62034b058a9b949a1f0635311206496c785a10b9
Andrei Iatan <andrei.iatan00@gmail.com>
2025-01-28 17:44:44 +0200

    initial commit

---
+++ src/test/java/com/to_do/main/ToDoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.to_do.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
