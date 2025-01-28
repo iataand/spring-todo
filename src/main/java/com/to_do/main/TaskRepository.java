da9a89b1ec0c47cf8f97ecf887118746e74e7ca4
Andrei Iatan <andrei.iatan00@gmail.com>
2025-01-28 17:59:32 +0200

    change into getTitle

--- src/main/java/com/to_do/main/TaskRepository.java
+++ src/main/java/com/to_do/main/TaskRepository.java
@@ -6,9 +6,9 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource(collectionResourceRel = "people", path = "people")
+@RepositoryRestResource(collectionResourceRel = "tasks", path = "tasks")
 public interface TaskRepository extends MongoRepository<Task, String> {
 
-  List<Task> findByLastName(@Param("name") String name);
+  List<Task> findByTitle(@Param("title") String title);
 
 }
